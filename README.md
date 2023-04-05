7840c0cd8bd8ffb754d140017a005b243d7cf281
alancrps <alanfcorpus@gmail.com>
2023-04-05 18:04:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ApiAlbums
+ 
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function saveAlbum(){
+    const userId = document.getElementById("userId").value;
+    const title = document.getElementById("title").value;
+
+    if (userId == 0 || title == ""){
+        return alert("los campos no pueden estar vacios")
+    }
+    
+}
